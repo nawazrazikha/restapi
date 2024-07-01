91f2eb681706dd5ddcf2364cca3595724e4712b8
Razikha <razikha.parveen@lunarasolutions.in>
2024-07-01 17:06:22 +0530

    postnew

--- src/components/AxiosPost.js
+++ src/components/AxiosPost.js
@@ -1,36 +1,41 @@
 import axios from "axios";
-import { useEffect, useState } from "react";
+import { useState } from "react";
 
-const AxiosPost=()=>{
- 
- const movie={
-    startDate:new Date(),
-    endDate:new Date().toTimeString()
- }
+const AxiosPost = () => {
+  const [fname, setFname] = useState('');
+  const [lname, setLname] = useState('');
 
- async function post(){
-    try{
-        const response=await axios.post('https://lunara-http-default-rtdb.firebaseio.com/movies.json',{
-            method:'POST',
-            body:JSON.stringify(movie),
-            headers:{
-                'Content-Type':'Application/json'
-            }
-        })
-        console.log(response)
-        
-    }catch(err){
-        console.log(err)
+  async function post() {
+    try {
+      const response = await axios.post('https://lunara-http-default-rtdb.firebaseio.com/details.json', {
+        fname: fname,
+        lname: lname
+      }, {
+        headers: {
+          'Content-Type': 'application/json'
+        }
+      });
+      console.log(response);
+    } catch (err) {
+      console.log(err);
     }
-    
-    
- }
- useEffect(()=>{
-    post()
- },[])
+  }
 
-return (
-    <div>post data</div>
-)
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    post();
+  }
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <h1>Post data</h1>
+      <label>First Name</label>
+      <input type="text" value={fname} onChange={(e) => setFname(e.target.value)} />
+      <label>Last Name</label>
+      <input type="text" value={lname} onChange={(e) => setLname(e.target.value)} />
+      <button type="submit">Submit</button>
+    </form>
+  );
 }
+
 export default AxiosPost;
